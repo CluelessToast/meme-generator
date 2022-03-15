d0383d5c7386b75f58cf29dac8b838152a1f9ba9
CluelessToast <88457405+CluelessToast@users.noreply.github.com>
2022-03-15 13:51:56 -0400

    meme project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="meme.png" className="header--img"  alt="" />
+        <h2 className="header--title">Header Component</h2>
+        <h4 className="header--project">React Course p3</h4>
+        </header>
+    )
+}
